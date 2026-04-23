dafc591372f9784f2dcd32f4085c9dc20626c0c9
Brad Gravesen <b@bradgravesen.com>
2026-04-23 09:10:45 -0700

    Comment out AWS provider and resource configurations in main.tf and outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
-output "instance_hostname" {
-  description = "Private DNS name of the EC2 instance."
-  value       = aws_instance.app_server.private_dns
-}
+# output "instance_hostname" {
+#   description = "Private DNS name of the EC2 instance."
+#   value       = aws_instance.app_server.private_dns
+# }
 
-output "instance_security_group_ids" {
-  value = aws_instance.app_server.vpc_security_group_ids
-}
+# output "instance_security_group_ids" {
+#   value = aws_instance.app_server.vpc_security_group_ids
+# }
 
-output "instance_subnet" {
-  value = aws_instance.app_server.subnet_id
-}
+# output "instance_subnet" {
+#   value = aws_instance.app_server.subnet_id
+# }
 
